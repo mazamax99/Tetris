0db9363b621ee50ba0a093607141938c2a2e1d2f
mazamax99 <ziperziperziperziper@gmail.com>
2019-05-17 19:28:43 +0600

    Добавление новых фигур

--- src/Figures.java
+++ src/Figures.java
@@ -28,4 +28,9 @@ public class Figures
             {0, 0, 0}}
     };
 
+    public static Figure createRandomFigure(int x,int y)
+    {
+        int index = (int) (Math.random() * 6);
+        return new Figure(x, y, BRICKS[index]);
+    }
 }
