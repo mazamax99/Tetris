08beb0349e367cf8c1c507e32ccd264012a26397
mazamax99 <ziperziperziperziper@gmail.com>
2019-05-17 19:26:06 +0600

    Создание методов движения

--- src/Tetris.java
+++ src/Tetris.java
@@ -3,8 +3,41 @@ import java.io.IOException;
 public class Tetris
 {
 
+    private Field field;
+    private Figure figure;
+
+    private boolean isGameOver;
+
+    public Tetris(int width, int height)
+    {
+        field = new Field(width, height);
+        figure = null;
+    }
+
+    public Field getField()
+    {
+        return field;
+    }
+
+    public Figure getFigure()
+    {
+        return figure;
+    }
+
+
+    public void setFigure(Figure figure)
+    {
+        this.figure = figure;
+    }
+
+    public void setField(Field field)
+    {
+        this.field = field;
+    }
+
+    public static Tetris game;
     public static void main(String[] args) throws Exception
     {
-
+        game = new Tetris(10, 20);
     }
 }
